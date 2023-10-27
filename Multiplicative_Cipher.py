b9d399ef21f9ecfdde057917ada69a4b1a2c59aa
prashanthm2410 <97015668+prashanthm2410@users.noreply.github.com>
2023-10-27 13:23:21 +0530

    Update Multiplicative_Cipher.py

--- Multiplicative_Cipher.py
+++ Multiplicative_Cipher.py
@@ -18,25 +18,30 @@ def extendedeuclid(key):
     t1 = 0
     t2 = 1
     while r2 > 0:
+        q = r1 / r2
         r = r1 % r2
-        q = (r1-r)/r2
         r1 = r2
         r2 = r
-        t = t1 * q - t2
+        t = t1 - q * t2
         t1 = t2
         t2 = t
-    return t1
+    if r1 == 1:
+        return t1
+    else:
+        return -1
 
 def decryption():
     ct = input("Enter the Encrypted message >> ")
-    key = extendedeuclid(int(input("Enter the key used for encryption >> ")))
+    k = int(input("Enter the key used for encryption >> "))
+    key = extendedeuclid(k)
+    if key == -1:
+        print(f"ERROR : Concurrent key for {k} does not exist...")
+        exit()
     pt = ""
     for i in ct:
         pos = 0
         pos = l.index(i)
         pt += l[(pos*key)%26]
     print(f"Original message is >> {pt}")
-
-
 encryption()
 decryption()
